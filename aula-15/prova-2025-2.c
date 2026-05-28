f09be3fb0b6172aeb8b015db61a1693d6258ddaa
Lucas Vinícius da Silva <lucasv2224@outlook.com>
2026-05-28 19:57:26 +0000

    atividade de struct

---
+++ aula-15/prova-2025-2.c
@@ -0,0 +1,177 @@
+#include <stdio.h>
+#include <string.h>
+
+#define TAM_NOME 20
+
+struct Produto{
+    int id;
+    char descricao[TAM_NOME];
+    int estoque;
+    float vlrUnitario;
+};
+
+struct Venda{
+    int id;
+    int qtdProdutosVendidos;
+    int idsProdutos[5];
+    int qtdsVendidas[5];
+};
+
+int main(){
+    return 0;
+}
+
+int buscaSequencialDesc(struct Produto v[], int tam, char x[]){
+    for(int i = 0; i < tam; i++){
+        if(strcmp(v[i].descricao, x) == 0){
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+int buscaBinariaId(struct Venda v[], int tam, int x){
+    int inicio = 0, fim = tam - 1;
+
+    while(inicio <= fim){
+        int meio = inicio + (fim - inicio) / 2;
+
+        if(v[meio].id == x){
+            return meio;
+        } else if(v[meio].id > x){
+            fim = meio - 1;
+        } else if(v[meio].id < x){
+            inicio = meio + 1;
+        }
+    }
+
+    return -1;
+}
+
+void lerStr(char str[], int tamMax){
+    fgets(str, tamMax, stdin);
+
+    int tam = strlen(str);
+
+    if(tam > 0 && str[tam - 1] == '\n'){
+        str[tam - 1] = '\0';
+    }
+}
+
+int alteraProduto(struct Produto v[], int tam){
+    char descricaoAltera[TAM_NOME];
+    char op;
+    int alterou = 0;
+
+    printf("Digite a descrição do produto que você deseja alterar: ");
+    lerStr(descricaoAltera, TAM_NOME);
+
+    for(int i = 0; i < tam; i++){
+        if(strcmp(v[i].descricao, descricaoAltera) == 0){
+            printf("Deseja alterar a descrição(s/n): ");
+            scanf("%c", &op);
+
+            if(op == 's'){
+                printf("Digite a nova descrição: ");
+                lerStr(v[i].descricao, TAM_NOME);
+                alterou += 1;
+            }
+
+            printf("Deseja alterar o estoque(s/n): ");
+            scanf("%c", &op);
+
+            if(op == 's'){
+                printf("Digite o novo estoque: ");
+                scanf("%d", &v[i].estoque);
+                alterou += 1;
+            }
+
+            printf("Deseja alterar o valor unitário(s/n): ");
+            scanf("%c", &op);
+
+            if(op == 's'){
+                printf("Digite o novo valor unitário: ");
+                scanf("%f", &v[i].vlrUnitario);
+                alterou += 1;
+            }
+        }
+
+        break;
+    }
+
+    if(alterou > 0){
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void venda(struct Produto vp[], int tamProd, struct Venda vv[], int tamVendas){
+    int idVenda, qtdProdutoDif, qtdVenda, achei, iProduto;
+    char descBusca[TAM_NOME];
+
+    printf("Digite o ID da venda: ");
+    scanf("%d", &idVenda);
+
+    for(int i = 0; i < tamVendas; i++){
+        if(vv[i].id == idVenda){
+            printf("Venda com esse ID já existe!\n");
+            return;
+        }
+    }
+
+    vv[tamVendas].id = idVenda;
+    vv[tamVendas].qtdProdutosVendidos = 0; // Começa com 0 itens inseridos
+
+    do{
+        printf("Digite a quantidade de produtos diferentes que deseja comprar(1 a 5): ");
+        scanf("%d", &qtdProdutoDif);
+    } while(qtdProdutoDif < 1 || qtdProdutoDif > 5);
+
+    for(int i = 0; i < qtdProdutoDif; i++){
+        achei = 0; // Reseta a flag para cada produto novo da lista
+
+        printf("\nDigite a descrição do produto %d que deseja comprar: ", i + 1);
+        lerStr(descBusca, TAM_NOME);
+
+        // CORREÇÃO 1: Laço interno para BUSCAR em todo o estoque de produtos
+        for(int j = 0; j < tamProd; j++){
+            if(strcmp(vp[j].descricao, descBusca) == 0){
+                iProduto = j; // Guarda o índice onde o produto real foi achado
+                achei = 1;
+                break; // Se achou, pode parar de procurar
+            }
+        }
+
+        if(achei == 1){
+            printf("Digite a quantidade de produtos %d que deseja comprar (Estoque: %d): ", i + 1, vp[iProduto].estoque);
+            scanf("%d", &qtdVenda);
+
+            if(qtdVenda >= 1 && qtdVenda <= vp[iProduto].estoque){
+                // 1. Deduz do estoque
+                vp[iProduto].estoque -= qtdVenda;
+                
+                // CORREÇÃO 2: Salva nos vetores usando a posição atual (0, 1, 2...)
+                int posAtual = vv[tamVendas].qtdProdutosVendidos;
+                
+                vv[tamVendas].idsProdutos[posAtual] = vp[iProduto].id;
+                vv[tamVendas].qtdsVendidas[posAtual] = qtdVenda;   // Faltava salvar a quantidade!
+                
+                // 3. Incrementa a quantidade de produtos inseridos nesta venda
+                vv[tamVendas].qtdProdutosVendidos++;
+                
+                printf("Produto adicionado com sucesso!\n");
+            } else {
+                printf("Quantidade inválida ou estoque insuficiente!\n");
+                i--; // Faz o loop repetir para o usuário tentar o produto i de novo
+            }
+        } else {
+            printf("Produto não encontrado!\n");
+            i--; // Faz o loop repetir para o usuário tentar o produto i de novo
+        }
+    }
+    
+    printf("\nVenda cadastrada com sucesso!\n");
+}
+
